e6dca4c10714bdd4b74e797758014671ca58d51f
lukew3 <lukew25073@gmail.com>
2023-11-02 14:02:55 -0400

    set iframe query on change

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
+const REPLII_BASE = 'http://localhost:3000/sidebar';
+
 function updateContent() {
 	chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
 	  // tabs[0] contains the currently active tab
-	  document.getElementById('url').textContent = tabs[0].url;
+	  const url = tabs[0].url;
+	  document.getElementById('url').textContent = url;
 	  document.getElementById('title').textContent = tabs[0].title;
+	  document.getElementById('repliiFrame').src = REPLII_BASE + '?url=' + encodeURIComponent(url);
 	});
 }
 
